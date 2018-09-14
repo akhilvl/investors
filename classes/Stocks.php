fdb3acf314105f2f1b316b02ff022a85d1b8aa0c
alohithakshan <alohithakshan@gracenote.com>
2018-09-14 17:26:58 +0530

    stock comments addded

--- classes/Stocks.php
+++ classes/Stocks.php
@@ -1,11 +1,30 @@
 <?php
+/**
+ * Stocks -> To manipulate with stock information
+ * 
+ * 
+ * @package    Stocks
+ * @subpackage Common
+ * @author     Akhil
+ */
+ 
 class Stocks extends Common {
 
+  /**
+   * To get the active stocks with the input stock-name
+   *
+   * @param String - $name -> Name of the stock
+   * @return Array -> Stock info
+   */
 	public function getStocksData($name){
 		$q = DB::mquery("select * from stocks where status=1");
 		return DB::fetch($q);
 	}
 	
+  /**
+   * To add stocks after full validation
+   *
+   */		
 	public function add_Stocks(){
 		$company_id = clean(Input::fetch('company_id'));
 		$exchange_id = clean(Input::fetch('exchange_id'));
@@ -45,7 +64,13 @@ class Stocks extends Common {
 			
 		}
 	}
+
 	
+  /**
+   * To get all active stocks
+   *
+   * @return Array -> All stocks info
+   */		
 	public function getAllStocks(){
 		$q = DB::mquery("select b.company_name,c.exchange_name,a.* from stocks as a 
 		inner join company as b on a.company_id=b.company_id 
@@ -54,6 +79,12 @@ class Stocks extends Common {
 		return DB::fetchAll($q);
 	}
 
+  /**
+   * Report 3: Highest trade value of each company stock
+   *
+   * @param Int - $company_id -> Id of the company
+   * @return Array -> company info
+   */	
 	public function getTopStockCompanyWise($company_id){
 		$q = DB::mquery("select * from stocks where company_id=$company_id and status=1 order by unit_price desc limit 1");
 		return DB::fetch($q);	
